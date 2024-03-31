768be2368ffcfe3032daae0130d6c94f1f835734
alokkumarsingh07 <kalok8841@gmail.com>
2024-03-31 13:16:01 +0530

    IsLoading

--- Movie-List/src/App.jsx
+++ Movie-List/src/App.jsx
@@ -1,27 +1,32 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
-
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMoviesHandler() {
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
       });
+
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Fetch operation failed:", error);
+    }
   }
 
   return (
@@ -30,7 +35,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found No Movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </>
   );
